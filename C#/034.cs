6a9148a843981dba4d5f53586a0ab922b3dc9f4c
rwhitworth <me@ryanwhitworth.com>
2015-08-07 23:17:03 -0400

    C# 034

---
+++ C#/034.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PE;
+
+namespace _034
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int64 sum = 0;
+
+            for (int i = 3; i <= 100000; i++)
+            {
+                String s = i.ToString();
+                Int64 answer = 0;
+                foreach (var item in s)
+                {
+                    answer += PE.Utilities.factorial(Int64.Parse(item.ToString()));
+                }
+                if (answer == i)
+                {
+                    sum += answer;
+                }
+            }
+
+            Console.WriteLine("{0}", sum);
+        }
+    }
+}
