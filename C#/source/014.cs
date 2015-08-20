c2a6c755ea6e549bdde9a3db95d1432e3baa776b
rwhitworth <me@ryanwhitworth.com>
2015-08-19 22:50:35 -0400

    C# 014

---
+++ C#/source/014.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _014
+{
+    class Program
+    {
+        static Int64 FindTerms(Int64 startnum)
+        {
+            Int64 tempnum = startnum;
+            Int64 counter = 1;
+            while (tempnum > 1)
+            {
+                if (tempnum % 2 == 0)
+                {
+                    tempnum = tempnum / 2;
+                }
+                else
+                {
+                    tempnum = (tempnum * 3) + 1;
+                }
+                counter++;
+            }
+            return counter;
+        }
+
+        static void Main(string[] args)
+        {
+            // n -> n/2 (n is even)
+            // n -> 3n + 1 (n is odd)
+            Int64 largest = 1;
+            Int64 largestterms = 1;
+            Int64 tempterms = 1;
+            for (Int64 i = 2; i < 1000000; i++)
+            {
+                tempterms = FindTerms(i);
+                if (tempterms > largestterms)
+                {
+                    largestterms = tempterms;
+                    largest = i;
+                }
+            }
+            Console.WriteLine(largest);
+        }
+    }
+}
