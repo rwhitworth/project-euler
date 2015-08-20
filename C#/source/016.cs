a8059d879120af3c023ad39c451208a02b28c912
rwhitworth <me@ryanwhitworth.com>
2015-08-19 22:54:00 -0400

    C# 016 025

---
+++ C#/source/016.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProjectEuler;
+
+namespace _016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInt b = new BigInt();
+            b = ProjectEuler.ProjectEuler.power(2, 1000);
+            char[] array = b.ToString().ToCharArray();
+            b = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                b += int.Parse(array[i].ToString());
+            }
+            Console.WriteLine(b.ToString());
+        }
+    }
+}
---
+++ C#/source/025.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProjectEuler;
+
+namespace _025
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInt b = 0;
+            for (int i = 4780; i < 5000; i++)
+            {
+                b = ProjectEuler.ProjectEuler.FibIterative(i);
+                if (b.ToString().Length == 1000)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+        }
+    }
+}
