111d55c64ba8fbc6e39404e68f448029986c466c
rwhitworth <me@ryanwhitworth.com>
2015-08-19 22:47:34 -0400

    C# 013, plus file renaming

---
+++ C#/source/010.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProjectEuler;
+
+namespace _10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int64 total = 2;
+            for (int i = 3; i < 2000000; i+=2)
+            {
+                if (ProjectEuler.ProjectEuler.Primetest(i))
+                {
+                    total += i;
+                }
+            }
+
+            Console.WriteLine(total);
+        }
+    }
+}
---
+++ C#/source/013.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProjectEuler;
+
+namespace _13
+{
+    class Program
+    {
+        static BigInt[] a = new BigInt[100];
+
+        static void PopulateArrayFromFile()
+        {
+            System.IO.StreamReader sr = new System.IO.StreamReader("13.txt");
+            int counter = 0;
+            while (!sr.EndOfStream)
+            {
+                a[counter] = BigInt.Parse(sr.ReadLine());
+                counter++;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            BigInt sum = 0;
+            PopulateArrayFromFile();
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                sum += a[i];
+            }
+
+            Console.WriteLine(sum.ToString().Substring(0, 10));
+        }
+
+    }
+}
