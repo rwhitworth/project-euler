1faf7404fb40f6d6c320f0ad7cf3ba397d8da047
rwhitworth <me@ryanwhitworth.com>
2015-08-19 22:37:02 -0400

    C# 001

---
+++ C#/source/001.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int64 count = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+
+                if (i % 3 == 0)
+                {
+                    count += i;
+                }
+                else
+                if (i % 5 == 0)
+                {
+                    count += i;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
