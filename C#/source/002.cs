2427e4e4a6642f09cfc272896058eef42f7d96ca
rwhitworth <me@ryanwhitworth.com>
2015-08-19 22:40:23 -0400

    C# 002

---
+++ C#/source/002.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProjectEuler;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            BigInt result = 0;
+            BigInt count = 0;
+            for (i = 1; i < 35; i++)
+            {
+                result = ProjectEuler.ProjectEuler.Fib(i);
+                if ((result % 2 == 0) && (result < 4000000))
+                {
+                    count += result;
+                }
+            }
+            Console.WriteLine(count.ToString());
+        }
+    }
+}
