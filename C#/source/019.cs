57f81d7a4844135c30d8162eb001b4b6ad78e2a4
rwhitworth <me@ryanwhitworth.com>
2016-01-31 22:09:29 -0500

    C# 019

---
+++ C#/source/019.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total_count = 0;
+            System.DateTime dt = new DateTime(1901, 1, 1);
+            while (dt.Year < 2001)
+            {
+                dt = dt.AddMonths(1);
+                if (dt.Day == 1 && dt.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    total_count++;
+                }
+            }
+            Console.WriteLine("{0}", total_count);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -20,6 +20,7 @@ Solved problems:<br>
 016 - C# and perl<br>
 017 - C#<br>
 018 - C#<br>
+019 - C#<br>
 020 - C# and perl<br>
 021 - C# and perl<br>
 025 - C# and perl<br>
