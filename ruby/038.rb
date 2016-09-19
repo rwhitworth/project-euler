ecf4048c70df291ed57060f10f879a3ded649bc2
Ryan Whitworth <me@ryanwhitworth.com>
2016-09-19 15:59:06 -0400

    ruby 038 minor style change (#5)

--- ruby/038.rb
+++ ruby/038.rb
@@ -5,5 +5,5 @@ class Fixnum
   end
 end
 
-puts 1.upto(10000).to_a.collect { |x| x.to_s + (2*x).to_s unless not (x.to_s + (2*x).to_s).to_i.pandigital? }.compact.reduce{ |largest, curr| curr.to_i if curr.to_i > largest.to_i }
+puts (1..10000).collect { |x| x.to_s + (2*x).to_s unless not (x.to_s + (2*x).to_s).to_i.pandigital? }.compact.reduce{ |largest, curr| curr.to_i if curr.to_i > largest.to_i }
 
