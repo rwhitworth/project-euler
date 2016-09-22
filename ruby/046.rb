680a8f652fe97627ee8e3c4bc1e2ed23f69dcdf9
Ryan Whitworth <me@ryanwhitworth.com>
2016-09-21 21:39:19 -0400

    ruby 046 style change

--- ruby/046.rb
+++ ruby/046.rb
@@ -1,5 +1,5 @@
 require 'prime'
 primes = 3.step(10_000,2).to_a.collect { |x| x if Prime.prime?(x) }.compact
 comp = 3.step(10_000,2).to_a.collect { |x| x if !Prime.prime?(x) }.compact
-res = 1.upto(10_000).to_a.collect { |c| primes.collect { |x| x + (2 * c * c) } }.flatten
-puts comp.collect { |x| x if !res.include?(x) }.compact.first
+res = (1..10000).flat_map { |c| primes.collect { |x| x + (2 * c * c) } }
+puts comp.detect { |x| x if !res.include?(x) }
